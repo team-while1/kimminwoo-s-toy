e1948b0b707c37c1d7d2a5859307fc89239c78be
김민우 <pilt@boxsunny.local>
2025-01-05 23:15:26 +0900

    Rendering Lists

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,8 @@
 // JSX : JavaScript XML
-function Message(){
-    const name = 'Minwoo';
-    if (name)
-        return <h1>Hello {name}</h1>
-    return <h1>Hello world</h1>;
+function Message() {
+  const name = "Minwoo";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello world</h1>;
 }
 
 export default Message;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,14 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   return (
     <Fragment>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </Fragment>
   );
