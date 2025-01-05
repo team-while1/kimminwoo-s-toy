85d78b1ff81102e3b0994f115e4e12d2e337f0ae
김민우 <pilt@boxsunny.local>
2025-01-05 23:28:22 +0900

    0105 DONE

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,22 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
+//38:36부터
 
 export default ListGroup;
