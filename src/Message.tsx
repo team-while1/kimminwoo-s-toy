c17cda678c9a8e387abdf1ff47725fb0644a7302
김민우 <pilt@boxsunny.local>
2025-01-05 22:49:17 +0900

    hello world

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+// JSX : JavaScript XML
+function Message(){
+    const name = 'Minwoo';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>;
+}
+
+export default Message;
